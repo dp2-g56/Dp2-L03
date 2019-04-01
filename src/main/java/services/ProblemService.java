9fb7c35662c4f4daf0669daf0f5f77fca8d90b4f
pabgarbar <44401034+pabgarbar@users.noreply.github.com>
2019-04-01 10:29:31 +0200

    Update ProblemService.java

--- src/main/java/services/ProblemService.java
+++ src/main/java/services/ProblemService.java
@@ -29,10 +29,6 @@ public class ProblemService {
 	@Autowired
 	private ProblemRepository	problemRepository;
 
-
-	@Autowired
-	private CompanyService		companyService;
-
 	@Autowired
 	private CompanyService		companyService;
 
@@ -63,7 +59,6 @@ public class ProblemService {
 	public void delete(Problem problem) {
 		this.problemRepository.delete(problem);
 	}
-
 
 	public List<Problem> showProblems() {
 		Company company = this.companyService.getLoggedCompany();
@@ -108,23 +103,12 @@ public class ProblemService {
 
 		problem.setAttachments(attachment);
 		problem.setTitle("");
-
-		problem.setIsDraftMode(null);
-
+		problem.setHint("")
 		problem.setIsDraftMode(true);
-
 		problem.setStatement("");
 
 		return problem;
 	}
-
-
-	public Problem addAttachment(String attachment, Problem problem) {
-
-		Assert.isTrue(!attachment.trim().isEmpty() && this.isUrl(attachment));
-
-		problem.getAttachments().add(attachment);
-		return this.save(problem);
 
 	public void addAttachment(String attachment, Problem problem) {
 		Assert.isTrue(this.companyService.loggedCompany().getProblems().contains(problem) && problem.getIsDraftMode());
